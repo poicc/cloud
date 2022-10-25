123ffddbcdb22f0373cdd06fced8ce3a4f5c2f93
陈蓉琪 <chenrongqi@njbandou.com>
2022-10-25 14:44:17 +0800

    搭建分布式事务项目demo

---
+++ spring-cloud-alibaba-distribution-demo/order-service-demo/src/test/java/com/crq/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package com.crq;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link com.crq.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
